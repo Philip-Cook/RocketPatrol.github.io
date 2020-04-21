1523dd7a6418d1733191ce21101636ba80b93296
Philip Cook <Pecook@ucsc.edu>
2020-04-20 17:12:37 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play  ],
+};
+
+let game = new Phaser.Game(config);
+
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000,
+}
+
+let keySPACE, keyLEFT, keyRIGHT;
